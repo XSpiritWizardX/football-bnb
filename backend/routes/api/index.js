79947be33833c8c6ff9cf6a4f98266e335096217
Dustin Bovee <xspiritwizardx@LAPTOP-DO4JCCG7>
2024-07-23 00:48:45 -0400

    added user authentication express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,13 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.use(restoreUser);
+
+
 
 
 
@@ -7,9 +15,60 @@ const router = require('express').Router();
 
 
 router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+  res.json({ requestBody: req.body });
+});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'Demo-lition'
+    }
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+
+
+
+
+
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+
+
+
+router.use(restoreUser);
+
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 
 
