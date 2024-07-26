4dcfc97b8b5edc9e2b056bd3bb97e2874f0538ab
Dustin Bovee <xspiritwizardx@LAPTOP-DO4JCCG7>
2024-07-25 20:02:14 -0400

    api documentation read me edit

--- backend/db/seeders/20240717002739-demo-user.js
+++ backend/db/seeders/20240717002739-demo-user.js
@@ -14,22 +14,43 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
-        firstName: 'James',
-        lastName: 'Thompson',
+        firstName: 'Dustin',
+        lastName: 'Bovee',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
-        username: 'FakeUser1',
-        firstName: 'James',
-        lastName: 'Thompson',
+        username: 'swordGuy2000',
+        firstName: 'Sokka',
+        lastName: 'Sokka',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
-        username: 'FakeUser2',
-        firstName: 'James',
-        lastName: 'Thompson',
+        username: 'metal-muscles',
+        firstName: 'Toph',
+        lastName: 'Beifong',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user3@user.io',
+        username: 'The-last-airbender',
+        firstName: 'Aang',
+        lastName: 'Aang',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user4@user.io',
+        username: 'BloodBender',
+        firstName: 'Katara',
+        lastName: 'Katara',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user5@user.io',
+        username: 'PrinceofFire',
+        firstName: 'Zuko',
+        lastName: 'Zuko',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], { validate: true });
