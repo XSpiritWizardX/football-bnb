b1f94ee2599fa3326c86bcd3ea649495aad83eee
XSpiritWizardX <dustinboveemusic@gmail.com>
2024-12-10 20:37:56 -0500

    render good now

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,11 +3,12 @@ const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.j
 const { User } = require('../../db/models');
 const sessionRouter = require('./session.js')
 const usersRouter = require('./users.js')
+const spotsRouter = require('./spots.js')
 
 router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
-
+router.use('/spots', spotsRouter);
 
 
 
