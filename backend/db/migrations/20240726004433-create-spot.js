426818349964c2f3ec0fb2170cc2e2dd82a33e1c
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-01-03 13:42:44 -0500

    router routes

--- backend/db/migrations/20240726004433-create-spot.js
+++ backend/db/migrations/20240726004433-create-spot.js
@@ -61,9 +61,9 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Spots'
-    await queryInterface.removeConstraint("avatar_bnb.Bookings", "Bookings_spotid_fkey")
-    await queryInterface.removeConstraint("avatar_bnb.Reviews", "Reviews_spotid_fkey")
-    await queryInterface.removeConstraint("avatar_bnb.SpotImages", "SpotImages_spotid_fkey")
+    // await queryInterface.removeConstraint("avatar_bnb.Bookings", "Bookings_spotid_fkey")
+    // await queryInterface.removeConstraint("avatar_bnb.Reviews", "Reviews_spotid_fkey")
+    // await queryInterface.removeConstraint("avatar_bnb.SpotImages", "SpotImages_spotid_fkey")
     await queryInterface.dropTable(options);
   }
 };
