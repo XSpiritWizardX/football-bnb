af2f203cd3b1c759c4a231c427acb10113ab2c18
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-01-03 21:51:52 -0500

    render bug fix 2.6

--- backend/db/migrations/20240726004433-create-spot.js
+++ backend/db/migrations/20240726004433-create-spot.js
@@ -63,10 +63,10 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     options.tableName = 'Spots'
     // await queryInterface.removeConstraint("avatar_bnb.Bookings", "Bookings_spotid_fkey")
-    // await queryInterface.removeConstraint("avatar_bnb.Reviews", "Reviews_spotid_fkey")
+    await queryInterface.removeConstraint("avatar_bnb.Reviews", "Reviews_spotid_fkey")
     // await queryInterface.removeConstraint("avatar_bnb.SpotImages", "SpotImages_spotid_fkey")
     // await queryInterface.dropTable('Bookings');
-    // await queryInterface.dropTable('Reviews');
+    await queryInterface.dropTable('Reviews');
     // await queryInterface.dropTable('SpotImages');
     // await queryInterface.dropTable('Spots');
     await queryInterface.dropTable(options);
