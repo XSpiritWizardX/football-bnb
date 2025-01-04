d4b6165d24f34b6d816c0568f50d88973c843c00
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-01-03 22:24:46 -0500

    render bug fix 2.8.12

--- backend/db/migrations/20240727180957-create-booking.js
+++ backend/db/migrations/20240727180957-create-booking.js
@@ -17,6 +17,7 @@ module.exports = {
       },
       spotId: {
         type: Sequelize.INTEGER,
+        allowNull: false,
         references:{
           model:"Spots",
           id:"id"
@@ -26,6 +27,7 @@ module.exports = {
       },
       userId: {
         type: Sequelize.INTEGER,
+        allowNull: false,
         references:{
           model:"Users",
           id:"id"
