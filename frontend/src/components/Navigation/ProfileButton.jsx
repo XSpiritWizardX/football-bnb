871414bf5f813fcd68452a7c178a697e42393f2c
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-01-28 13:35:09 -0500

    added football profile button and Nav bar

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,13 @@
+import { GiAmericanFootballBall } from "react-icons/gi";
+
+// ...
+
+const Football = () => {
+  return (
+    <div style={{ color: "brown", fontSize: "100px" }}>
+     <GiAmericanFootballBall />
+    </div>
+  );
+};
+
+export default Football
