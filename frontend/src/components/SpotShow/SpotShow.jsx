355a6c956b291907fae3a53fd8422419de0ace2f
XSpiritWizardX <dustinboveemusic@gmail.com>
2025-02-21 22:54:22 -0500

    added review on spot show for frontend

--- frontend/src/components/SpotShow/SpotShow.jsx
+++ frontend/src/components/SpotShow/SpotShow.jsx
@@ -4,15 +4,16 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchOneSpot } from "../../store/spots";
 import {fetchReviews} from "../../store/reviews"
-import { MdOutlineStar } from "react-icons/md";
+import { MdOutlineStar} from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import './SpotShow.css'
 
 
 
 const SpotShow = () => {
+
   const dispatch = useDispatch();
-  const user = useSelector((state) => state.session.user)
+  // const user = useSelector((state) => state.session.user)
   const spots = useSelector((state) => state.spots.spot || []);
   const reviews = useSelector((state) => state.reviews.reviews?.Reviews || []);
   const { spotId } = useParams()
@@ -20,13 +21,36 @@ const SpotShow = () => {
 
   const reviewElements = [];
 
+
+
   for (let i = 0; i < reviews.length; i++) {
     reviewElements.push(
       <div key={reviews[i].id}
-      className="review2"      >
-        <div
-        className="review3"
-        >{user?.firstName}{user?.lastName}: {reviews[i].review}</div>
+      className="review2"
+      >
+        <div>
+
+          <h3>
+          {reviews?.[i]?.User?.firstName} {reviews?.[i]?.User?.lastName}
+
+          </h3>
+
+          <h3>
+           {new Date(reviews?.[i]?.createdAt).toLocaleString('default', { month: 'long' })} <ln>
+
+           </ln>
+           {new Date(reviews?.[i]?.createdAt).getFullYear()}
+          </h3>
+
+          <p
+          className="review-text"
+          >
+          {reviews[i].review}
+
+          </p>
+
+          {/* {deleteButton} */}
+        </div>
       </div>
     );
   }
@@ -252,7 +276,10 @@ const SpotShow = () => {
 
 
               <div
+
+
               className="reviews-container"
+
               >
 
 
@@ -261,15 +288,26 @@ const SpotShow = () => {
                   <div
                   className="avgerage-rating-big"
                   >
+
+                  <div
+                  className="big-star"
+                  >
+
                     <MdOutlineStar />
                     {!spots?.avgRating ? "New" : spots?.avgRating}
 
-                    {/*
-                    ^^^^^^^  -- if it doesnt have an average rating,
-                    it should display (New)
-                    #Reviews:{spots?.reviews.length}
+                  </div>
 
-                    */}
+                        <span
+                        className="seperation"
+                        >
+                          &#183;
+                        </span>
+
+
+                      {!reviews ? "  New" : `  ${reviews.length} reviews`}
+
+
                   </div>
 
 
@@ -296,7 +334,9 @@ const SpotShow = () => {
             //  key={reviews?.id}
              >
 
-              <p>{reviewElements}</p>
+              <div
+              className="div-of-divs"
+              >{reviewElements}</div>
 
              </div>
 
